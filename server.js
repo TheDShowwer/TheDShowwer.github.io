7844bb5ba7f1ee762178f1249ef53748208eb6db
TheDShowwer <90554949+TheDShowwer@users.noreply.github.com>
2023-06-02 20:48:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,66 +13,51 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/api/player/:platform/:name', async (req, res) => {
+app.get('/api/player/:platform/:id', async (req, res) => {
   const platform = req.params.platform;
-  const name = req.params.name;
-  const apiKey = process.env.API_KEY;
+  const id = req.params.id;
+  const apiKey = process.env.BUNGIE_API_KEY;
 
   try {
     // Fetch player data using Bungie API
-    const response = await axios.get(`https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/${platform}/${name}/`, {
+    const response = await axios.get(`https://www.bungie.net/Platform/Destiny2/${platform}/Profile/${id}/?components=100`, {
       headers: {
         'X-API-Key': apiKey
       }
     });
     const data = response.data;
 
-    if (data.Response && data.Response.length > 0) {
-      const membershipId = data.Response[0].membershipId;
-      const destinyMembershipId = data.Response[0].membershipId;
+    if (data.Response) {
+      const profile = data.Response.profile.data;
 
-      // Fetch player profile data using membershipId and destinyMembershipId
-      const profileResponse = await axios.get(`https://www.bungie.net/Platform/Destiny2/${membershipId}/Profile/${destinyMembershipId}/?components=100`, {
+      // Extract necessary data from the profile
+      const emblem = profile.emblemPath;
+      const classType = profile.classType;
+      const race = profile.raceType;
+      const lightLevel = profile.light;
+
+      // Fetch most used weapons/armor
+      const itemsResponse = await axios.get(`https://www.bungie.net/Platform/Destiny2/${platform}/Profile/${id}/?components=102`, {
         headers: {
           'X-API-Key': apiKey
         }
       });
-      const profileData = profileResponse.data;
+      const itemsData = itemsResponse.data;
 
-      if (profileData.Response) {
-        const profile = profileData.Response.profile.data;
+      if (itemsData.Response) {
+        const items = itemsData.Response.profileInventory.data.items;
 
-        // Extract necessary data from the profile
-        const emblem = profile.emblemPath;
-        const classType = profile.classType;
-        const race = profile.raceType;
-        const lightLevel = profile.light;
+        // Filter items by rarity
+        const rarityFilter = req.query.rarity; // Expects a query parameter named 'rarity'
+        const filteredItems = items.filter(item => item.itemTypeAndTierDisplayName.toLowerCase().includes(rarityFilter));
 
-        // Fetch most used weapons/armor
-        const itemsResponse = await axios.get(`https://www.bungie.net/Platform/Destiny2/${membershipId}/Profile/${destinyMembershipId}/?components=102`, {
-          headers: {
-            'X-API-Key': apiKey
-          }
-        });
-        const itemsData = itemsResponse.data;
-
-        if (itemsData.Response) {
-          const items = itemsData.Response.profileInventory.data.items;
-
-          // Filter items by rarity
-          const rarityFilter = req.query.rarity; // Expects a query parameter named 'rarity'
-          const filteredItems = items.filter(item => item.itemTypeAndTierDisplayName.toLowerCase().includes(rarityFilter));
-
-          // Return the player data and filtered items as JSON
-          res.json({ emblem, classType, race, lightLevel, filteredItems });
-        } else {
-          res.status(404).json({ error: 'Player items not found' });
-        }
+        // Return the player data and filtered items as JSON
+        res.json({ emblem, classType, race, lightLevel, filteredItems });
       } else {
-        res.status(404).json({ error: 'Player profile not found' });
+        res.status(404).json({ error: 'Player items not found' });
       }
     } else {
-      res.status(404).json({ error: 'Player not found' });
+      res.status(404).json({ error: 'Player profile not found' });
     }
   } catch (error) {
     console.error('Error:', error);
