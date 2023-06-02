7c8dd9386945aa21bd713f6c0bc9569181969379
TheDShowwer <90554949+TheDShowwer@users.noreply.github.com>
2023-06-02 17:08:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -94,5 +94,56 @@ function displayPlayerData(data) {
   const armorContainer = document.createElement('div');
   armorContainer.classList.add('usage-container');
   armorContainer.innerHTML = `
-    <
+    <h3>Most Used Armor</h3>
+    <div id="armor-filter" class="filter">
+      <label for="armor-type">Type:</label>
+      <select id="armor-type">
+        <option value="">All</option>
+        <option value="Helmet">Helmet</option>
+        <option value="Gauntlets">Gauntlets</option>
+        <option value="Chest Armor">Chest Armor</option>
+        <option value="Leg Armor">Leg Armor</option>
+        <option value="Class Item">Class Item</option>
+      </select>
+    </div>
+    <div id="armor-list"></div>
+  `;
+  resultContainer.appendChild(armorContainer);
+
+  const armorList = document.getElementById('armor-list');
+
+  // Apply filter when the armor type selection changes
+  const armorTypeSelect = document.getElementById('armor-type');
+  armorTypeSelect.addEventListener('change', () => {
+    const selectedType = armorTypeSelect.value;
+    filterArmorByType(selectedType);
+  });
+
+  // Display all armor initially
+  data.armor.forEach((armor) => {
+    const armorElement = document.createElement('div');
+    armorElement.classList.add('armor');
+    armorElement.innerHTML = `
+      <h4>${armor.name}</h4>
+      <p>Type: ${armor.type}</p>
+      <p>Equipped: ${armor.equipped ? 'Yes' : 'No'}</p>
+    `;
+    armorList.appendChild(armorElement);
+  });
+
+  function filterArmorByType(type) {
+    const allArmor = Array.from(armorList.getElementsByClassName('armor'));
+    allArmor.forEach((armorElement) => {
+      if (!type || armorElement.querySelector('p[data-type]').getAttribute('data-type') === type) {
+        armorElement.style.display = 'block';
+      } else {
+        armorElement.style.display = 'none';
+      }
+    });
+  }
+}
+
+function displayErrorMessage(message) {
+  resultContainer.innerHTML = `<p class="error">${message}</p>`;
+}
      
