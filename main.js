42457bddd15f0ad4e436e6d4de7133782f7191d8
TheDShowwer <90554949+TheDShowwer@users.noreply.github.com>
2023-06-02 17:07:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,98 @@
-function searchPlayer() {
-  const playerName = document.getElementById('playerName').value;
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const platformSelect = document.getElementById('platform-select');
+const resultContainer = document.getElementById('result-container');
 
-  fetch(`/player/${playerName}`)
-    .then(response => response.json())
-    .then(data => {
-      const playerInfo = document.getElementById('playerInfo');
-      playerInfo.innerHTML = `
-        <h2>${data.displayName}</h2>
-        <img src="${data.emblem}" alt="Player Emblem">
-        <p>Light Level: ${data.lightLevel}</p>
-        <p>Class: ${data.class}</p>
-      `;
-    })
-    .catch(error => console.error(error));
-}
+searchForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const playerName = searchInput.value.trim();
+  const platform = platformSelect.value;
+
+  if (!playerName) {
+    return;
+  }
+
+  try {
+    const response = await fetch(`/api/profile?playerName=${encodeURIComponent(playerName)}&platform=${platform}`);
+    const data = await response.json();
+
+    displayPlayerData(data);
+  } catch (error) {
+    console.error(error);
+    displayErrorMessage('An error occurred while fetching player data.');
+  }
+});
+
+function displayPlayerData(data) {
+  resultContainer.innerHTML = '';
+
+  // Display player information
+  const playerInfo = document.createElement('div');
+  playerInfo.innerHTML = `
+    <h2>${data.displayName}</h2>
+    <p>Platform: ${data.platform}</p>
+    <p>Emblem: ${data.emblem}</p>
+    <p>Class: ${data.class}</p>
+    <p>Race: ${data.race}</p>
+    <p>Light Level: ${data.lightLevel}</p>
+  `;
+  resultContainer.appendChild(playerInfo);
+
+  // Display most used guns
+  const gunsContainer = document.createElement('div');
+  gunsContainer.classList.add('usage-container');
+  gunsContainer.innerHTML = `
+    <h3>Most Used Guns</h3>
+    <div id="guns-filter" class="filter">
+      <label for="gun-rarity">Rarity:</label>
+      <select id="gun-rarity">
+        <option value="">All</option>
+        <option value="Exotic">Exotic</option>
+        <option value="Legendary">Legendary</option>
+        <option value="Rare">Rare</option>
+        <option value="Common">Common</option>
+      </select>
+    </div>
+    <div id="guns-list"></div>
+  `;
+  resultContainer.appendChild(gunsContainer);
+
+  const gunsList = document.getElementById('guns-list');
+
+  // Apply filter when the rarity selection changes
+  const gunRaritySelect = document.getElementById('gun-rarity');
+  gunRaritySelect.addEventListener('change', () => {
+    const selectedRarity = gunRaritySelect.value;
+    filterGunsByRarity(selectedRarity);
+  });
+
+  // Display all guns initially
+  data.guns.forEach((gun) => {
+    const gunElement = document.createElement('div');
+    gunElement.classList.add('gun');
+    gunElement.innerHTML = `
+      <h4>${gun.name}</h4>
+      <p>Rarity: ${gun.rarity}</p>
+      <p>Kills: ${gun.kills}</p>
+    `;
+    gunsList.appendChild(gunElement);
+  });
+
+  function filterGunsByRarity(rarity) {
+    const allGuns = Array.from(gunsList.getElementsByClassName('gun'));
+    allGuns.forEach((gunElement) => {
+      if (!rarity || gunElement.querySelector('p[data-rarity]').getAttribute('data-rarity') === rarity) {
+        gunElement.style.display = 'block';
+      } else {
+        gunElement.style.display = 'none';
+      }
+    });
+  }
+
+  // Display most used armor
+  const armorContainer = document.createElement('div');
+  armorContainer.classList.add('usage-container');
+  armorContainer.innerHTML = `
+    <
+     
