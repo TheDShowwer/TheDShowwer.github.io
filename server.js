848cb4e002d3a8e6aaeade4a6a04422a10a823e2
TheDShowwer <90554949+TheDShowwer@users.noreply.github.com>
2023-06-02 21:06:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,72 +1,47 @@
 const express = require('express');
-const path = require('path');
-require('dotenv').config();
 const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-// Serve the HTML file for the root URL
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/api/player/:platform/:id', async (req, res) => {
-  const platform = req.params.platform;
-  const id = req.params.id;
-  const apiKey = process.env.BUNGIE_API_KEY;
-
+app.post('/player', async (req, res) => {
   try {
-    // Fetch player data using Bungie API
-    const response = await axios.get(`https://www.bungie.net/Platform/Destiny2/${platform}/Profile/${id}/?components=100`, {
+    const bungieName = req.body.bungieName;
+    const apiKey = process.env.BUNGIE_API_KEY;
+    const searchUrl = 'https://www.bungie.net/Platform/User/SearchUsers/';
+    const searchParams = {
+      search: bungieName
+    };
+
+    const response = await axios.post(searchUrl, searchParams, {
       headers: {
-        'X-API-Key': apiKey
+        'X-API-Key': apiKey,
+        'Content-Type': 'application/json'
       }
     });
-    const data = response.data;
 
-    if (data.Response) {
-      const profile = data.Response.profile.data;
+    // Assuming the first profile in the response is the desired player
+    const playerProfile = response.data.Response[0];
 
-      // Extract necessary data from the profile
-      const emblem = profile.emblemPath;
-      const classType = profile.classType;
-      const race = profile.raceType;
-      const lightLevel = profile.light;
+    // Get the Destiny membership ID and membership type
+    const destinyMembershipId = playerProfile.membershipId;
+    const membershipType = playerProfile.membershipType;
 
-      // Fetch most used weapons/armor
-      const itemsResponse = await axios.get(`https://www.bungie.net/Platform/Destiny2/${platform}/Profile/${id}/?components=102`, {
-        headers: {
-          'X-API-Key': apiKey
-        }
-      });
-      const itemsData = itemsResponse.data;
-
-      if (itemsData.Response) {
-        const items = itemsData.Response.profileInventory.data.items;
-
-        // Filter items by rarity
-        const rarityFilter = req.query.rarity; // Expects a query parameter named 'rarity'
-        const filteredItems = items.filter(item => item.itemTypeAndTierDisplayName.toLowerCase().includes(rarityFilter));
-
-        // Return the player data and filtered items as JSON
-        res.json({ emblem, classType, race, lightLevel, filteredItems });
-      } else {
-        res.status(404).json({ error: 'Player items not found' });
-      }
-    } else {
-      res.status(404).json({ error: 'Player profile not found' });
-    }
+    res.json({
+      destinyMembershipId,
+      membershipType
+    });
   } catch (error) {
-    console.error('Error:', error.response?.data || error.message);
-    res.status(500).json({ error: 'Server error' });
+    console.log('Error:', error.response.data);
+    res.status(500).json({ error: 'Failed to retrieve player membership ID' });
   }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 
